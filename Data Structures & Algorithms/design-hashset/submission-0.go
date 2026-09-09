c4ae710df586080a59f75d0c9dca893f6d05820d
Niroop R <niroopparker@gmail.com>
2026-09-09 22:21:19 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.go
@@ -0,0 +1,39 @@
+type MyHashSet struct {
+	data []int
+}
+
+func Constructor() MyHashSet {
+    return MyHashSet {data: []int{}}
+}
+
+func (this *MyHashSet) Add(key int) {
+    if(!this.Contains(key)) {
+		this.data=append(this.data,key)
+	}
+}
+
+func (this *MyHashSet) Remove(key int) {
+    for i,v:=range this.data {
+		if v==key {
+			this.data=append(this.data[:i],this.data[i+1:]...)
+		}
+	}
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+    for _,v:=range this.data {
+		if v==key {
+			return true
+		}
+	}
+	return false
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
+ 
