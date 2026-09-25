861d6ab6c063548c318528aae9715a4c3595a1ab
Niroop R <niroopparker@gmail.com>
2026-09-25 21:44:34 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.go
@@ -0,0 +1,25 @@
+func checkInclusion(s1 string, s2 string) bool {
+	if len(s1)>len(s2){
+		return false
+	}
+	count1,count2:=[26]int{},[26]int{}
+
+	for i:=range s1 {
+		count1[s1[i]-'a']=count1[s1[i]-'a']+1
+		count2[s2[i]-'a']=count2[s2[i]-'a']+1
+	}
+
+	if count1==count2 {
+		return true
+	}
+
+	for i:=len(s1);i<len(s2);i++{
+
+		count2[s2[i]-'a']=count2[s2[i]-'a']+1
+		count2[s2[i-len(s1)]-'a']=count2[s2[i-len(s1)]-'a']-1
+		if count1==count2 {
+		return true
+	}
+	}
+	return false
+}
